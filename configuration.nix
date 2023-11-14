13e58527abce9a8748a50d060ad19855d48d4bd5
Pavel Goroshko <mcredpower@gmail.com>
2023-11-14 23:21:59 +0100

    init conf

---
+++ configuration.nix
@@ -0,0 +1,194 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.kernelPackages = pkgs.linuxPackages_6_5;
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      useOSProber = true;
+    };
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+  };
+
+  # network
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+    services.xserver = {
+        layout = "us";
+        xkbVariant = "";
+       # enable = true;
+       # videoDrivers = ["nvidia"];
+       # displayManager.sddm = {
+       #    enable = true;
+       #    theme = "maya";
+       #    autoNumlock = true;
+       # };
+    };
+
+  
+  # printers
+  services.printing.enable = true;
+  
+  # sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver = {
+    videoDrivers = ["nvidia"];
+    enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+  };
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  };
+
+  # Enabling hyprlnd on NixOS
+  programs.hyprland = {
+    enable = true;
+    nvidiaPatches = true;
+    xwayland = {
+      enable = true;
+      hidpi = true;
+    };
+  };
+
+  environment.sessionVariables = {
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NIXOS_OZONE_WL = "1";
+  };
+
+
+  # user
+  users.users.drembryo = {
+    isNormalUser = true;
+    description = "DrEmbryo";
+    extraGroups = [ "networkmanager" "wheel" "kvm" "video"];
+    packages = with pkgs; [
+      firefox
+      kate
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "drembryo";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # packages
+  environment.systemPackages = with pkgs; [
+     kitty
+     wget
+     neofetch
+     git
+     hyprland
+     hyprpaper
+     waybar
+     dunst
+     libnotify
+     wofi
+     sddm
+     xdg-desktop-portal-hyprland
+     xdg-desktop-portal-gtk
+     xdg-utils
+     xwayland
+  ];
+
+  fonts.fontDir.enable = true;
+  fonts.fonts = with pkgs; [  
+    nerdfonts
+    font-awesome
+    google-fonts
+  ];
+
+  services.dbus.enable = true;
+
+  # window portals
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # firewall
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # overlays
+  nixpkgs.overlays = [
+    (self: super: {
+      waybar = super.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      });
+    })
+   ];
+
+  # version
+  system.stateVersion = "23.05";
+}
